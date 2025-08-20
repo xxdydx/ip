af613f5ea016aef63af488f2eeb7b08c5effdf63
Arul <official@arul.me>
2025-08-20 17:53:32 +0800

    feat: level-0 progress

---
+++ src/main/java/Lyra.java
@@ -0,0 +1,13 @@
+public class Lyra {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println(" Hello! I'm Lyra");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
+
+
