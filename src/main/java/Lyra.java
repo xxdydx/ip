b44e47e5ac7c19050a2a85c3e948cfb57418f891
Arul <official@arul.me>
2025-08-20 17:58:08 +0800

    feat: level-1 progress

--- src/main/java/Lyra.java
+++ src/main/java/Lyra.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Lyra {
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
+        final String line = "____________________________________________________________";
+
         System.out.println(line);
         System.out.println(" Hello! I'm Lyra");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            String trimmedInput = input.trim();
+
+            System.out.println();
+            if (trimmedInput.equalsIgnoreCase("bye")) {
+                System.out.println(line);
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+
+            System.out.println(line);
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
 
