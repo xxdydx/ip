56a84a54fe571243aa4de32d1ff711371854cab3
Arul <official@arul.me>
2025-08-30 18:41:19 +0200

    Add JavaFX GUI support with Launcher and DialogBox (#9)

---
+++ src/main/java/lyra/DialogBox.java
@@ -0,0 +1,60 @@
+package lyra;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+/**
+ * DialogBox class for displaying user and bot messages in the GUI.
+ */
+public class DialogBox extends HBox {
+
+    private Label text;
+    private ImageView displayPicture;
+
+    public DialogBox(Label l, ImageView iv) {
+        text = l;
+        displayPicture = iv;
+
+        text.setWrapText(true);
+        text.setFont(Font.font("Arial", 12));
+        text.setMinHeight(Label.USE_PREF_SIZE);
+        
+        displayPicture.setFitWidth(50.0);
+        displayPicture.setFitHeight(50.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.setSpacing(10);
+        this.setPadding(new javafx.geometry.Insets(10));
+        this.getChildren().addAll(text, displayPicture);
+    }
+
+    /**
+     * Flips the dialog box such that the ImageView is on the left and text on the right.
+     */
+    private void flip() {
+        this.setAlignment(Pos.TOP_LEFT);
+        ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
+        FXCollections.reverse(tmp);
+        this.getChildren().setAll(tmp);
+    }
+
+    public static DialogBox getUserDialog(String text, boolean isUser) {
+        var db = new DialogBox(new Label(text), new ImageView());
+        db.text.setStyle("-fx-background-color: #e3f2fd; -fx-background-radius: 10; -fx-padding: 10;");
+        db.flip();
+        return db;
+    }
+
+    public static DialogBox getDukeDialog(String text, boolean isUser) {
+        var db = new DialogBox(new Label(text), new ImageView());
+        db.text.setStyle("-fx-background-color: #f3e5f5; -fx-background-radius: 10; -fx-padding: 10;");
+        return db;
+    }
+}
---
+++ src/main/java/lyra/GuiUi.java
@@ -0,0 +1,166 @@
+package lyra;
+
+import lyra.task.Task;
+import lyra.task.TaskList;
+import lyra.util.Ui;
+
+import java.util.ArrayList;
+
+/**
+ * GUI-compatible UI class that captures output as strings instead of printing to console.
+ * This allows the GUI to display messages without using System.out.println.
+ */
+public class GuiUi extends Ui {
+    private StringBuilder outputBuffer;
+
+    /**
+     * Constructs a new GuiUi instance.
+     */
+    public GuiUi() {
+        this.outputBuffer = new StringBuilder();
+    }
+
+    /**
+     * Gets the current output buffer content.
+     */
+    public String getOutput() {
+        return outputBuffer.toString();
+    }
+
+    /**
+     * Clears the output buffer.
+     */
+    public void clearOutput() {
+        outputBuffer.setLength(0);
+    }
+
+    // Override methods from Ui to capture output instead of printing
+    @Override
+    public void showWelcome() {
+        String welcome = "Hello! I'm Lyra\nWhat can I do for you?";
+        outputBuffer.append(welcome);
+    }
+
+    @Override
+    public void showGoodbye() {
+        String goodbye = "Bye. Hope to see you again soon!";
+        outputBuffer.append(goodbye);
+    }
+
+    @Override
+    public void showLine() {
+        String line = "____________________________________________________________";
+        outputBuffer.append(line);
+    }
+
+    @Override
+    public void showEmptyCommandMessage() {
+        String message = "Please enter a command. Type 'list', 'todo', 'deadline', 'event', 'mark', 'unmark', 'delete', or 'bye'.";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showUnknownCommandMessage() {
+        String message = "Sorry, I couldn't recognize that command. Try one of: list, todo, deadline, event, mark, unmark, delete, bye.";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showTaskList(ArrayList<Task> tasks) {
+        StringBuilder message = new StringBuilder();
+        if (tasks.isEmpty()) {
+            message.append("There are no tasks in your list.");
+        } else {
+            message.append("Here are the tasks in your list:\n");
+            for (int i = 0; i < tasks.size(); i++) {
+                message.append((i + 1)).append(".").append(tasks.get(i).toString()).append("\n");
+            }
+        }
+        String result = message.toString().trim();
+        outputBuffer.append(result);
+    }
+
+    @Override
+    public void showMatchingTasks(ArrayList<Task> tasks) {
+        StringBuilder message = new StringBuilder();
+        if (tasks.isEmpty()) {
+            message.append("No matching tasks found.");
+        } else {
+            message.append("Here are the matching tasks in your list:\n");
+            for (int i = 0; i < tasks.size(); i++) {
+                message.append((i + 1)).append(".").append(tasks.get(i).toString()).append("\n");
+            }
+        }
+        String result = message.toString().trim();
+        outputBuffer.append(result);
+    }
+
+    @Override
+    public void showTaskAdded(Task task, int totalTasks) {
+        String message = "Got it. I've added this task:\n  " + task.toString() + "\nNow you have " + totalTasks + " tasks in the list.";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showTaskMarked(Task task, boolean isDone) {
+        String message;
+        if (isDone) {
+            message = "Nice! I've marked this task as done:\n  " + task.toString();
+        } else {
+            message = "OK, I've marked this task as not done yet:\n  " + task.toString();
+        }
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showTaskDeleted(Task task, int remainingTasks) {
+        String message = "Noted. I've removed this task:\n  " + task.toString() + "\nNow you have " + remainingTasks + " tasks in the list.";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showInvalidTaskNumber() {
+        String message = "Invalid task number.";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showTodoErrorMessage() {
+        String message = "Sorry, a todo needs a description. Try: todo <description>";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showDeadlineErrorMessage() {
+        String message = "Sorry, a deadline needs a description and '/by'. Try: deadline <description> /by <when>";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showEventErrorMessage() {
+        String message = "Sorry, an event needs a description and times. Try: event <description> /from <start> /to <end>";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showDeleteErrorMessage() {
+        String message = "Please specify a task number to delete. Try: delete <task_number>";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showLoadingError() {
+        String message = "Error loading tasks from file. Starting with empty task list.";
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void showError(String message) {
+        outputBuffer.append(message);
+    }
+
+    @Override
+    public void close() {
+        // No need to close scanner in GUI
+    }
+}
